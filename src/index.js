2e35cc1c37bac28722cd0ac05273427926961548
Arnelle Balane <arnellebalane@gmail.com>
2019-05-24 01:39:36 +0800

    Create ApolloClient and make it available with ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import './index.css';
 
+const client = new ApolloClient({
+  uri: 'https://instagram-graphql.arnelle.me'
+});
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
